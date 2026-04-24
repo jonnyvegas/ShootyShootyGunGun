46b2bf420b5781ce392ed825295fcf90295f927d
THE-BEAST\jonat <jonathan.e.villegas@gmail.com>
2026-04-24 14:18:51 -0700

    Setup Robot class with a Transform for Target. Also getting component on awake for the NavMeshAgent. For now, set to move towards target on update so it chases. Set up in level.

---
+++ ShootyShootyGunGun/Assets/Scripts/Nav/Robot.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Robot : MonoBehaviour
+{
+    NavMeshAgent agent;
+    [SerializeField] Transform target;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //agent.SetDestination(target.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
