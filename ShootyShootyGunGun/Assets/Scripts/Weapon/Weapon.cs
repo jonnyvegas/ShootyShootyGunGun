67257cd920dc2ec7304e7e9689cc5f99e4067d82
THE-BEAST\jonat <jonathan.e.villegas@gmail.com>
2026-04-28 14:58:10 -0700

    Removed recovery unused scenes. Added materials from Asset Store. Added particle to BarrelLocation (where we traced from). Added code to fire particle system when firing gun, no matter if we hit something or not. Adjusted a bit. Looks OK but could be tweaked a bit more.

--- ShootyShootyGunGun/Assets/Scripts/Weapon/Weapon.cs
+++ ShootyShootyGunGun/Assets/Scripts/Weapon/Weapon.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+
     [SerializeField] Transform CameraLoc;
+    [SerializeField] ParticleSystem muzzleFlash;
+    
     RaycastHit hit;
     StarterAssetsInputs starterAssetsInputs;
     float shootDistance = 10000f;
@@ -15,20 +18,32 @@ public class Weapon : MonoBehaviour
         damage = gameObject.AddComponent<Damage>();
     }
 
+    private void CheckHit(RaycastHit hit)
+    {
+        if (hit.collider.TryGetComponent(out Health health))
+        {
+            health.SetHealth(health.CurrentHealth - damage.Dmg);
+        }
+    }
+
+    private void Shoot()
+    {
+        //Debug.DrawRay(CameraLoc.position, CameraLoc.forward * shootDistance, Color.red, 100f);
+        if (Physics.Raycast(CameraLoc.position, CameraLoc.forward, out hit, shootDistance))
+        {
+            CheckHit(hit);
+
+        }
+        starterAssetsInputs.ShootInput(false);
+        muzzleFlash.Play();
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (starterAssetsInputs.shoot)
         {
-            //Debug.DrawRay(CameraLoc.position, CameraLoc.forward * shootDistance, Color.red, 100f);
-            if (Physics.Raycast(CameraLoc.position, CameraLoc.forward, out hit, shootDistance))
-            {
-                if(hit.collider.TryGetComponent(out Health health))
-                {
-                    health.SetHealth(health.CurrentHealth - damage.Dmg);
-                }
-            }
-            starterAssetsInputs.ShootInput(false);
+            Shoot();
         }
     }
 }
