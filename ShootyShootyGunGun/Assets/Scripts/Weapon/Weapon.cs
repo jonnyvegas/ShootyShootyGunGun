c7e73aff0e85365e5b20bb0f7ce01875a1f9d1c7
THE-BEAST\jonat <jonathan.e.villegas@gmail.com>
2026-05-07 13:43:17 -0700

    Added Weapon Scriptable Object with FireRate and Damage fields. Added 3 types, Pistol, Machine Gun, and Sniper Rifle. These check in code how much damage to do. Added code to check for cooldown once the weapon is fired before allowing firing again.

--- ShootyShootyGunGun/Assets/Scripts/Weapon/Weapon.cs
+++ ShootyShootyGunGun/Assets/Scripts/Weapon/Weapon.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] WeaponSO weaponSO;
     [SerializeField] Transform BarrelLoc;
     [SerializeField] Transform CameraLoc;
     [SerializeField] ParticleSystem muzzleFlash;
@@ -12,12 +13,14 @@ public class Weapon : MonoBehaviour
     RaycastHit hit;
     StarterAssetsInputs starterAssetsInputs;
     float shootDistance = 50000f;
-    Damage damage;
+    //Damage damage;
     const string SHOOT_STRING = "Shoot";
+    float aggregTime = 0f;
+    bool canShoot = true;
     private void Awake()
     {
         starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
-        damage = gameObject.AddComponent<Damage>();
+        //damage = gameObject.AddComponent<Damage>();
     }
 
     private void CheckHit(RaycastHit hit)
@@ -29,30 +32,58 @@ public class Weapon : MonoBehaviour
         }
         if (hit.collider.TryGetComponent(out Health health))
         {
-            health.SetHealth(health.CurrentHealth - damage.Dmg);
+            health.SetHealth(health.CurrentHealth - weaponSO.Damage);
         }
+    }
+
+    private void PlayAnimAndFX()
+    {
+        muzzleFlash.Play();
+        animator.Play(SHOOT_STRING, 0, 0f);
     }
 
     private void Shoot()
     {
+        if (!canShoot)
+        {
+            return;
+        }
+        // start the timer check for cooldown.
+        canShoot = false;
         starterAssetsInputs.ShootInput(false);
-        muzzleFlash.Play();
-        animator.Play(SHOOT_STRING, 0, 0f);
+        PlayAnimAndFX();
         //Debug.Log(shootDistance);
         //Debug.DrawRay(BarrelLoc.position, CameraLoc.forward * 10000f, Color.red, 100f);
         if (!Physics.Raycast(BarrelLoc.position, CameraLoc.forward, out hit, shootDistance))
         {
             return;
         }
-        CheckHit(hit);  
+        CheckHit(hit);
+        
+    }
+
+    private void CheckCooldown()
+    {
+        aggregTime += Time.deltaTime;
+        if (aggregTime >= weaponSO.FireRate)
+        {
+            canShoot = true;
+            aggregTime = 0f;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!canShoot)
+        {
+            CheckCooldown();
+            return;
+        }
         if (starterAssetsInputs.shoot)
         {
             Shoot();
         }
+        
     }
 }
---
+++ ShootyShootyGunGun/Assets/Scripts/Weapon/WeaponSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponSO", menuName = "Scriptable Objects/WeaponSO")]
+public class WeaponSO : ScriptableObject
+{
+    public float Damage = 1f;
+    public float FireRate = 0.5f;
+}
