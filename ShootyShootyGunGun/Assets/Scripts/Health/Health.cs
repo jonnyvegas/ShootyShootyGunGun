37ef53d9287c0bc6ce61d7f64b44d14c68ea1944
THE-BEAST\jonat <jonathan.e.villegas@gmail.com>
2026-05-20 17:33:46 -0700

    Separated ZeroHealth into interface where we use a function to get it. We then bind/add a listener to it and get the interface reference and call it. Loose coupling.

--- ShootyShootyGunGun/Assets/Scripts/Health/Health.cs
+++ ShootyShootyGunGun/Assets/Scripts/Health/Health.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Health : MonoBehaviour
+public interface IHealth
+{
+    public UnityEvent<int> GetZeroHealthEvent() { return null; }
+}
+
+public class Health : MonoBehaviour, IHealth
 {
     [SerializeField] protected float health = 100f;
     float startingHealth = 100f;
     public float CurrentHealth => health;
 
-    public event System.Action ZeroHealthEvent;
+    public UnityEvent<int> ZeroHealthEvent = new UnityEvent<int>();
 
     public void SetHealth(float health)
     {
         this.health = health;
-        Debug.Log("Current health: " + this.health);
+        //Debug.Log("Current health: " + this.health);
         if(health <= 0 )
         {
             OnZeroHealth();
@@ -20,11 +26,18 @@ public class Health : MonoBehaviour
 
     virtual public void OnZeroHealth()
     {
-        ZeroHealthEvent?.Invoke();
+        // passing starting health as a test to make sure
+        // this is being done correctly.
+        ZeroHealthEvent?.Invoke((int)startingHealth);
     }
 
     public void ResetHealth()
     {
         health = startingHealth;
+    }
+
+    public UnityEvent<int> GetZeroHealthEvent()
+    {
+        return ZeroHealthEvent;
     }
 }
