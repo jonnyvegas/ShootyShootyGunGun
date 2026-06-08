1acb790d0945f032bf5822be676b2e9f3b4a9cdd
THE-BEAST\jonat <jonathan.e.villegas@gmail.com>
2026-06-08 15:31:40 -0700

    Added functionality/UI for enemies remaining. Added ref for GameManager and functions to update the enemies left and the YouWin text. Added functions in the enemy health to get the game manager and set enemies to +1. On death, set to -1. Added check in manager to see if enemies < 0 when checking enemies. Adjusting enemies adjusts the count the manager is looking for.

--- ShootyShootyGunGun/Assets/Scripts/Health/EnemyHealth.cs
+++ ShootyShootyGunGun/Assets/Scripts/Health/EnemyHealth.cs
@@ -2,13 +2,23 @@ using UnityEngine;
 
 public class EnemyHealth : Health
 {
+    GameManager gameManager;
+    
     private void Awake()
     {
         //health = 50f;
     }
+
+    private void Start()
+    {
+        gameManager = FindAnyObjectByType<GameManager>();
+        gameManager.AdjustEnemiesLeft(1);
+    }
+
     public override void OnZeroHealth()
     {
         base.OnZeroHealth();
+        gameManager.AdjustEnemiesLeft(-1);
         SelfDestruct();
     }
 
--- ShootyShootyGunGun/Assets/Scripts/Misc/GameManager.cs
+++ ShootyShootyGunGun/Assets/Scripts/Misc/GameManager.cs
@@ -1,17 +1,34 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] TMP_Text enemiesLeftText;
+    [SerializeField] GameObject youWinText;
+    const string ENEMIES_LEFT = "Enemies Left: ";
+    int enemiesLeft = 0;
+
     public void RestartLevelButton()
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentScene);
+        youWinText.SetActive(false);
     }
 
     public void QuitButton()
     {
         Debug.LogWarning("This does not work in the Unity Editor! Will work in build tho. KEKW");
         Application.Quit();
+    }
+
+    public void AdjustEnemiesLeft(int amt)
+    {
+        enemiesLeft += amt;
+        enemiesLeftText.text = ENEMIES_LEFT + enemiesLeft;
+        if(enemiesLeft <= 0)
+        {
+            youWinText.SetActive(true);
+        }
     }
 }
